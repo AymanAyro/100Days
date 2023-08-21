2f787ceae601d64284366e05d8793ebd7ed81941
Ayman Saber Gad <121309009+AymanAyro@users.noreply.github.com>
2023-08-22 00:44:41 +0300

    Add files via upload
    This is the project of day 3 at the "100 Days of Code: The Complete Python Pro Bootcamp for 2023" course by Angela Yu, it's a simple multi-choice short game made by conditioning statements (if/elif/else) in Python, I hope you enjoy it!

--- Day3_Project/Treasure _Island.py
+++ Day3_Project/Treasure _Island.py
@@ -22,25 +22,26 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-decision1 = input("You stand in fron of two roads, would you go left or right? (L/R) ")
-if decision1 == "L" or "l":
-    decision2 = input("At the end of the road you found a lake and across the lake there's a huge golden door, would you swim or wait? (S/W) ")
-    if decision2 == "W" or "w":
+decision1 = input("You stand in front of two roads, would you go left or right? (L/R) ")
+if decision1 == "L" or decision1 == "l":
+    decision2 = input("At the end of the road you found a lake and across it there's a huge golden door, would you swim or wait? (S/W) ")
+    if decision2 == "W" or decision2 == "w":
         decision3 = input("After a few seconds of waiting a wall to your left opened up, when you went inside you found 3 doors: Red, Blue, and Yellow, which would you choose? (R/B/Y) ")
-        if decision3 == 'R' or 'r':
+        if decision3 == 'R' or decision3 =='r':
             print("As you entered the room the door closed behind you and flames began to rise from every corner, you were burnt. GAME OVER. ")
-        elif decision3 == "B" or 'b':
+        elif decision3 == "B" or decision3 == 'b':
             print("You entered the room and started hearing weird voices'Grrr! Grrr!' getting closer, as you lit your torch you found yourself surrounded by beasts, you were eaten. GAME OVER.")
-        elif decision3 == "Y" or 'y':
+        elif decision3 == "Y" or decision3 == 'y':
             print("There was something glowing brightly in the center of the room, you kept approaching slowly until you found a box made of gold, it was the treasure filled with jewelry. Congrats, You WIN!!.")
         else:
             print("You didn't choose any of the rooms and kept standing until the snakes surrounded your legs and they attacked you, you died. GAME OVER. (Honestly though, what were you thinking just roaming around?)")
-    elif decision2 == "S" or 's':
+    elif decision2 == "S" or decision2 == 's':
         print("As soon as you jumped into the lake you saw a swarm of pirhanas rushing towards you, you were eaten. GAME OVER.")
     else:
         print("You stayed still until a venomous snake bit your leg, you lost control, and fell into the lake. GAME OVER. (Just give a real choice ok?)")
-elif decision1 == "R" or 'r':
+elif decision1 == "R" or decision1 == 'r':
     print("There was a hole at the end of the road, you fell and died. GAME OVER.")
 else:
-    print("You stood there taking so long to think what to choose until a crocodile appeared from behind and attacked you, you died. GAME OVER.")
+    print("You stood there taking so long to think what to choose until a crocodile appeared from behind and attacked you, you died. GAME OVER. (Umm, Why?)")
 
+input(" ")
